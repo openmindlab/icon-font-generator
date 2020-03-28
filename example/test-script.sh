73fb5881dc960902c201943901cd62aaacbc727b
Andrea Galbiati <andrea.galbiati@openmindonline.it>
2020-03-28 22:10:54 +0100

    fixed example params

--- example/test-script.sh
+++ example/test-script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-icon-font-generator sample/icons/*.svg -o sample/fonts --name test-icons --html=true -j=false --types 'svg, ttf, woff, woff2, eot' --csspath sample/css/_var_icons.scss --cssfontsurl ../fonts/ --skipHashNames=true
+icon-font-generator example/icons/*.svg -o example/fonts --name test-icons --html=true -j=false --types 'svg, ttf, woff, woff2, eot' --csspath example/css/_var_icons.scss --cssfontsurl ../fonts/ --skipHashNames=true
